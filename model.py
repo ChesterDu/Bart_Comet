b1744b1d412389045c0189ed06f7b67cdc7ad4fa
ChesterDu <40878339+ChesterDu@users.noreply.github.com>
2020-08-17 22:53:20 +0800

    add directory

--- model.py
+++ model.py
@@ -1,9 +1,10 @@
 from transformers import BartForConditionalGeneration
 import torch
+
+
 def make_model(model_pth = None):
     model = BartForConditionalGeneration.from_pretrained('facebook/bart-large')
     if model_pth != None:
         print("model loaded from {}".format(model_pth))
         model.load_state_dict(torch.load(model_pth))
-    
     return model
